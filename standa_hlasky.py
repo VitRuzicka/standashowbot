2e84b1f5e441ffc65abf6866c54c852f2416c8b3
VitRuzicka <36342077+VitRuzicka@users.noreply.github.com>
2018-12-09 10:59:49 +0100

    compatible with python3 now

--- standa_hlasky.py
+++ standa_hlasky.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
-import urllib2
-url = "https://twitch.center/customapi/quote/list?token=80580068"
+import urllib3
+url ='https://twitch.center/customapi/quote/list?token=80580068'
 
-def hlaska(cislo):
-	response = urllib2.urlopen(url)
-	data = response.read()
-	citat = data.split('\n')
-	#print citat[76] #cislo v zavorce urcuje kolikaty citat v poradi je vypsan 
-	if cislo > len(citat) - 1:
-		return citat[int(cislo) - 1]
+def hlaska(x):
+	http_pool = urllib3.connection_from_url(url)
+	r = http_pool.urlopen('GET',url)
+	hlasky = r.data.decode('utf-8').split('\n')
+	if int(x) < (len(hlasky) - 1):
+		return hlasky[int(x) - 1]
 	else:
-		return "neplatne_cislo"
-
+		return "neplatne cislo"
 
 def cislo(retezec):
 	for znak in retezec.split():
